f77574c75b8796c202facd9371e6a5e4a5ead2ee
Guilherme <guilhermeggr23@gmail.com>
2024-09-19 19:48:48 -0300

    Initial Commit

---
+++ src/main/java/com/streamwise/StreamWiseApplication.java
@@ -0,0 +1,13 @@
+package com.streamwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamWiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamWiseApplication.class, args);
+    }
+
+}
