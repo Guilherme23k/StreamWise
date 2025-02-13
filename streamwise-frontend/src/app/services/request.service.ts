6f528dd36122b9129daf6eeb6fb2092b8a5dc50f
Guilherme <guilhermeggr23@gmail.com>
2025-02-13 18:51:21 -0300

    feat: Adicionado lógica de req http post de usuarios

--- streamwise-frontend/src/app/services/request.service.ts
+++ streamwise-frontend/src/app/services/request.service.ts
@@ -1,21 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserJwtResponse } from '../types/user-jwt-response';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() {}
+  private apiUrl = 'http://localhost:8080/auth';
 
-  login() {
-    localStorage.setItem('isAuthenticated', 'true');
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string){
+    return this.http.post<UserJwtResponse>(this.apiUrl + "/login", {email, password}).pipe(
+      tap((value) => {
+        sessionStorage.setItem("username", value.name);
+        sessionStorage.setItem("auth-token", value.token);
+      })
+    )
   }
 
-  logout() {
-    localStorage.removeItem('isAuthenticated');
-  }
-
-  checkAuth(): boolean {
-    return localStorage.getItem('isAuthenticated') === 'true';
+  register(name: string, email: string, password: string){
+    return this.http.post<UserJwtResponse>(this.apiUrl + "/register", {name, email, password}).pipe(
+      tap((value) => {
+        sessionStorage.setItem("username", value.name);
+      })
+    )
   }
 }
