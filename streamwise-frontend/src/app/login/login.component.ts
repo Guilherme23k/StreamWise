901914d9efce6c737a67f963a8daaeb75270ebea
Guilherme <guilhermeggr23@gmail.com>
2025-02-13 20:12:10 -0300

    fix: Removendo lógica do componente login para altera-lo depois

--- streamwise-frontend/src/app/login/login.component.ts
+++ streamwise-frontend/src/app/login/login.component.ts
@@ -1,25 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../services/request.service';
+import { RequestUserService } from '../services/request.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule], // Adicione o FormsModule
+  imports: [CommonModule, FormsModule], 
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
-  loginData = { email: '', password: '' };
-
-  constructor(private authService: AuthService, private router: Router) {}
-
-  onLogin() {
-    
-    this.authService.login();  
-    this.router.navigate(['/home']);
-  }
+  
 }
