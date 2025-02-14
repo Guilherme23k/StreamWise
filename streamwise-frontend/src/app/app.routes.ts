0ef5a8cc69062f9cf393c190e41d2c1f2db3335b
Guilherme <guilhermeggr23@gmail.com>
2025-02-13 22:34:19 -0300

    feat: Adicionado components que irao auxiliar o login/register components

--- streamwise-frontend/src/app/app.routes.ts
+++ streamwise-frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FormLayoutComponent } from './components/form-layout/form-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: FormLayoutComponent}
+];
