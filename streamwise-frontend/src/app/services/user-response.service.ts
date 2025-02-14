2b8ebb6b14800699c8fb22353cdf901ae7723874
Guilherme <guilhermeggr23@gmail.com>
2025-02-13 23:05:01 -0300

    feat: Adicionado servico de POST de login/register

---
+++ streamwise-frontend/src/app/services/user-response.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UserResponseJWT } from '../types/user-response';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserResponseService {
+
+  private apiUrl = 'http://localhost:8080/auth';
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string){
+    return this.http.post<UserResponseJWT>(this.apiUrl + "/login", {email,password}).pipe(
+      tap((value) =>{
+        sessionStorage.setItem("username", value.name);
+        sessionStorage.setItem("auth-token", value.token)
+      })
+    )
+  }
+
+  register(name: string, email: string, password:string){
+    return this.http.post<UserResponseJWT>(this.apiUrl + "/register", {name, email, password}).pipe(
+      tap((value) => {
+        sessionStorage.setItem("username", value.name);
+      })
+    )
+  }
+}
