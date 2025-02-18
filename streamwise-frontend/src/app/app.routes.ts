a122ebacc9ff489949f797331ece183e40c40d71
Guilherme <guilhermeggr23@gmail.com>
2025-02-18 20:14:53 -0300

    feat: Adicionado rota de registro

--- streamwise-frontend/src/app/app.routes.ts
+++ streamwise-frontend/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { authGuard } from './services/guards/auth-guard.guard';
 import { AlreadyAuthGuard } from './services/guards/already-auth.guard';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
     {path: 'login', component: LoginComponent, canActivate: [AlreadyAuthGuard]},
+    {path: 'register', component: RegisterComponent, canActivate: [AlreadyAuthGuard]},
     {path: 'home', component: HomeComponent, canActivate: [authGuard]}
 ];
