c6b1a73065c2b356a2f47436aa4b143ed4e921f5
Guilherme <guilhermeggr23@gmail.com>
2025-02-18 19:52:20 -0300

    feat: Adicionado o already-auth na rota de login

--- streamwise-frontend/src/app/app.routes.ts
+++ streamwise-frontend/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { authGuard } from './services/guards/auth-guard.guard';
+import { AlreadyAuthGuard } from './services/guards/already-auth.guard';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
+    {path: 'login', component: LoginComponent, canActivate: [AlreadyAuthGuard]},
     {path: 'home', component: HomeComponent, canActivate: [authGuard]}
 ];
