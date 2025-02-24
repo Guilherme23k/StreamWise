be39ea609e3cc4b58d5670d6187212b7b63b9ec2
Guilherme <guilhermeggr23@gmail.com>
2025-02-24 18:56:12 -0300

    feat: Adicionado navbar provisoria da pagina principal

--- streamwise-frontend/src/app/pages/home/home.component.ts
+++ streamwise-frontend/src/app/pages/home/home.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  constructor(private router: Router){}
+
+  isMenuOpen = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  logout(){
+    sessionStorage.clear();
+    this.router.navigate(['/login']);
+  }
 
 }
