4706d5f724d2fd835415ee311530c52d1921fd9a
Guilherme <guilhermeggr23@gmail.com>
2025-02-24 20:15:58 -0300

    feat: Adicionado lista de cards que ira mostrar as assinaturas do usuario

--- streamwise-frontend/src/app/components/card/card.component.ts
+++ streamwise-frontend/src/app/components/card/card.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataCardService } from '../../services/data-card.service';
+import { CommonModule } from '@angular/common';
+
+interface Signature {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
+
+  signatures: Signature[] = [];
+  loading = true;
+  error = false;
+
+  constructor(private dataCardService: DataCardService){}
+
+  ngOnInit(): void {
+      this.dataCardService.getSignatures().subscribe({
+        next: (data) => {
+        this.signatures = data;
+        this.loading = false;
+      },
+      error: () => {
+        this.error = true;
+        this.loading = false;
+      }
+      })
+  }
 
 }
