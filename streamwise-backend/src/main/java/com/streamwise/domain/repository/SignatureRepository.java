a7c5b7fcc4d72a3a1751ef7f1e645b0990db6528
Guilherme <guilhermeggr23@gmail.com>
2025-02-25 21:16:15 -0300

    feat: Adicionado query manual que ira buscar a assinatura pelo ID do usuario

--- streamwise-backend/src/main/java/com/streamwise/domain/repository/SignatureRepository.java
+++ streamwise-backend/src/main/java/com/streamwise/domain/repository/SignatureRepository.java
@@ -2,10 +2,17 @@ package com.streamwise.domain.repository;
 
 import com.streamwise.domain.model.Signature;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SignatureRepository extends JpaRepository<Signature, Long> {
+
+    @Query("SELECT s FROM Signature s WHERE s.user.id = :userId")
+    List<Signature> findByUserId(@Param("userId") Long userId);
 
     boolean existsByUserIdAndNameAndCategory(Long userId, String name, String category);
 }
