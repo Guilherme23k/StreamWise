46796857dd09b406c97a34a590f3f10996858e72
Guilherme <guilhermeggr23@gmail.com>
2025-02-26 22:06:22 -0300

    refactor: Altera o endpoint do card de /user para /user/signatures para coincidir com o backend

--- streamwise-frontend/src/app/services/data-card.service.ts
+++ streamwise-frontend/src/app/services/data-card.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class DataCardService {
 
-  private apiUrl = 'http://localhost:8080/signatures'
+  private apiUrl = 'http://localhost:8080/signatures/user/signatures'
 
   constructor(private http: HttpClient) { }
 
