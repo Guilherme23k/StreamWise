1143bb6069243c2e00d615d1688e4571cda75e49
Guilherme <guilhermeggr23@gmail.com>
2025-03-07 19:46:09 -0300

    feat: Implementa conteudo do enum SignatureImage

--- streamwise-backend/src/main/java/com/streamwise/domain/model/SignatureImage.java
+++ streamwise-backend/src/main/java/com/streamwise/domain/model/SignatureImage.java
@@ -1,4 +1,17 @@
 package com.streamwise.domain.model;
 
 public enum SignatureImage {
+    NETFLIX("https://img.icons8.com/?size=100&id=20519&format=png&color=000000"),
+    PRIMEVIDEO("https://img.icons8.com/?size=100&id=Rs68BrhxH0XZ&format=png&color=000000");
+
+    private final String url;
+
+    SignatureImage(String url){
+        this.url = url;
+    }
+
+    public String getUrl(){
+        return url;
+    }
+
 }
