8654ee8593aa8e8fd624be3d8831ea3394cbfd7e
Guilherme <guilhermeggr23@gmail.com>
2025-03-10 19:53:38 -0300

    refactor: Adiciona nome do display da imagem da assinatura para melhorar experiencia no front

--- streamwise-backend/src/main/java/com/streamwise/domain/model/SignatureImage.java
+++ streamwise-backend/src/main/java/com/streamwise/domain/model/SignatureImage.java
@@ -1,17 +1,23 @@
 package com.streamwise.domain.model;
 
 public enum SignatureImage {
-    NETFLIX("https://img.icons8.com/?size=100&id=20519&format=png&color=000000"),
-    PRIMEVIDEO("https://img.icons8.com/?size=100&id=Rs68BrhxH0XZ&format=png&color=000000");
+    NETFLIX("Netflix", "https://img.icons8.com/?size=100&id=20519&format=png&color=000000"),
+    PRIMEVIDEO("Prime Video","https://img.icons8.com/?size=100&id=Rs68BrhxH0XZ&format=png&color=000000");
 
+    private final String displayName;
     private final String url;
 
-    SignatureImage(String url){
+    SignatureImage(String displayName, String url){
         this.url = url;
+        this.displayName = displayName;
     }
 
     public String getUrl(){
         return url;
+    }
+
+    public String getDisplayName() {
+        return displayName;
     }
 
 }
