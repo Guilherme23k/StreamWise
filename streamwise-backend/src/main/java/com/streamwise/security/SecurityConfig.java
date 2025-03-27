5da7dc45455a22253fbab2726a439de1e04cbca4
Guilherme Gonçalves Ramos <98847472+Guilherme23k@users.noreply.github.com>
2025-03-27 00:06:45 -0300

    Update SecurityConfig.java

--- streamwise-backend/src/main/java/com/streamwise/security/SecurityConfig.java
+++ streamwise-backend/src/main/java/com/streamwise/security/SecurityConfig.java
@@ -57,14 +57,15 @@ public class SecurityConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
+    CorsConfiguration configuration = new CorsConfiguration();
+    
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://streamwise-ggr.netlify.app"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+    configuration.setAllowCredentials(true);  
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration); 
+    return source;
+}
 }
