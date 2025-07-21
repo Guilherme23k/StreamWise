716447c02a4b82e60866dc0bf035b6ab4a074c31
Guilherme <guilhermeggr23@gmail.com>
2025-07-21 20:30:12 -0300

    configs iniciasi pra rodar localmente

--- streamwise-frontend/src/app/services/data-card.service.ts
+++ streamwise-frontend/src/app/services/data-card.service.ts
@@ -7,13 +7,8 @@ import { Observable } from 'rxjs';
 })
 export class DataCardService {
 
-<<<<<<< HEAD
-  private apiUrlGET = 'https://streamwise-production.up.railway.app/signatures/user/me/signatures';
-  private apiUrl = 'https://streamwise-production.up.railway.app/signatures';
-=======
-  private readonly apiUrlGET = 'https://streamwise-production.up.railway.app/signatures/user/me/signatures';
-  private readonly apiUrl = 'https://streamwise-production.up.railway.app/signatures';
->>>>>>> c159579a24bcbfcd41b245b9d1373f35ddb47260
+  private apiUrlGET = 'http://localhost:8080/signatures/user/me/signatures';
+  private apiUrl = 'http://localhost:8080/signatures';
 
   constructor(private http: HttpClient) { }
 
