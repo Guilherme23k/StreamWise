a14091f7840d4e2c5c7d8a2a7e7bd065905e88bc
Guilherme <guilhermeggr23@gmail.com>
2025-08-26 21:44:38 -0300

    feat: adiciona lista de streamings previamente carregados

---
+++ streamwise-frontend/src/app/types/streamingList.ts
@@ -0,0 +1,13 @@
+
+import { Streaming } from "../interface/Streaming"
+
+export const STREAMINGS: Streaming[] = [
+
+    { name: 'Netflix', image: 'https://img.icons8.com/?size=100&id=20519&format=png&color=000000' },
+    { name: 'Prime Video', image: 'https://img.icons8.com/?size=100&id=Rs68BrhxH0XZ&format=png&color=000000' },
+    { name: 'Disney Plus', image: 'https://img.icons8.com/?size=100&id=PIBuqGDN2nPm&format=png&color=000000' },
+    { name: 'HBO Max', image: 'https://img.icons8.com/?size=100&id=9tVdlpWe1F9k&format=png&color=000000' },
+    { name: 'Paramont', image: 'https://img.icons8.com/?size=100&id=Ggt3oh6aoRWh&format=png&color=000000' },
+    { name: 'Globoplay', image: 'https://img.icons8.com/?size=100&id=cdS20W1QGgDD&format=png&color=000000' }
+
+]
