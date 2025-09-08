047a3c8e86e1fc83e079950c3a224b3bfc7de494
Guilherme <guilhermeggr23@gmail.com>
2025-09-08 20:28:11 -0300

    fix: re-adiciona o implements UserDetails pois o Usercontroller faz validacoes com ele

--- streamwise-backend/src/main/java/com/streamwise/domain/model/User.java
+++ streamwise-backend/src/main/java/com/streamwise/domain/model/User.java
@@ -1,15 +1,18 @@
 package com.streamwise.domain.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 
 
+import java.util.Collection;
 import java.util.List;
 
 
 @Entity(name = "tb_users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -66,7 +69,37 @@ public class User {
     }
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 }
