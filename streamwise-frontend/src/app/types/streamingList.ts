7e61143186a6fadd71cfcacb170a4096683f167c
Guilherme <guilhermeggr23@gmail.com>
2025-11-28 00:29:53 -0300

    fix: arruma as url das imagens que nao estavam sendo carregadas

--- streamwise-frontend/src/app/types/streamingList.ts
+++ streamwise-frontend/src/app/types/streamingList.ts
@@ -3,14 +3,14 @@ export interface StreamingSelect {
   image: string;
 }
 
-// Lista de streaming pra escolha (mock) - só pra modal
+
 export const STREAMINGS_SELECT: StreamingSelect[] = [
-  { name: 'Netflix', image: 'https://img.icons8.com/?size=100&id=...' },
-  { name: 'Prime Video', image: 'https://img.icons8.com/?size=100&id=...' },
-  { name: 'Disney Plus', image: 'https://img.icons8.com/?size=100&id=...' },
-  { name: 'HBO Max', image: 'https://img.icons8.com/?size=100&id=...' },
-  { name: 'Paramont', image: 'https://img.icons8.com/?size=100&id=...' },
-  { name: 'Globoplay', image: 'https://img.icons8.com/?size=100&id=...' },
+  { name: 'Netflix', image: 'https://img.icons8.com/?size=100&id=20519&format=png&color=000000' },
+    { name: 'Prime Video', image: 'https://img.icons8.com/?size=100&id=Rs68BrhxH0XZ&format=png&color=000000' },
+    { name: 'Disney Plus', image: 'https://img.icons8.com/?size=100&id=PIBuqGDN2nPm&format=png&color=000000' },
+    { name: 'HBO Max', image: 'https://img.icons8.com/?size=100&id=9tVdlpWe1F9k&format=png&color=000000' },
+    { name: 'Paramont', image: 'https://img.icons8.com/?size=100&id=Ggt3oh6aoRWh&format=png&color=000000' },
+    { name: 'Globoplay', image: 'https://img.icons8.com/?size=100&id=cdS20W1QGgDD&format=png&color=000000' }
 ];
 
 export const codeToName: Record<string, string> = {
