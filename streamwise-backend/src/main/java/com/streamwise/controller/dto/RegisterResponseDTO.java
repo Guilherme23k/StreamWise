a748cea04fdd56ba4a6a54bd1b03fd3f07373ed2
Guilherme <guilhermeggr23@gmail.com>
2025-12-09 20:51:52 -0300

    refactor: altera record do RegisterResponseDTO de boolean enabled para email quando for feito o registro

--- streamwise-backend/src/main/java/com/streamwise/controller/dto/RegisterResponseDTO.java
+++ streamwise-backend/src/main/java/com/streamwise/controller/dto/RegisterResponseDTO.java
@@ -2,10 +2,10 @@ package com.streamwise.controller.dto;
 
 import com.streamwise.domain.model.User;
 
-public record RegisterResponseDTO(String name, Boolean enabled) {
+public record RegisterResponseDTO(String name, String email) {
 
     public static RegisterResponseDTO fromUser(User user){
-        return new RegisterResponseDTO(user.getName(), user.isEnabled());
+        return new RegisterResponseDTO(user.getName(), user.getEmail());
     }
 
 }
